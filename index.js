10e44cf69ad87763d8b383cccea868df8276104a
kateobrien <k8obrien2811@gmail.com>
2021-09-18 22:45:55 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return`This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(responsibility = "go to the office"){
+    return `This Monday, I will ${responsibility}.`;
+}
+mondayWork("pet my cat")
+
+function wrapAdjective(wrap = '*'){
+     return function(word = 'special'){
+        return `You are ${wrap}${word}${wrap}!`
+    }
+}
